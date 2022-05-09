98da5067ead97678e3cb2d9202655ef459ddb333
Juan Pablo Gomez de los Santos <juanpa.gomez@gruposantander.com>
2022-05-09 18:13:49 +0200

    [JP] Initial Version

---
+++ terraform/itdmodcomm001/staitdjpgcomm004/dev/dev.tfvars
@@ -0,0 +1,23 @@
+# Storage
+
+sta_name                            = "sgtd2weustaitdjpgcomm004"
+lwk_name                            = "sgtd2weulwkitdjpgcomm004"
+kvt_name                            = "sgtd2weuakvitdmodcomm002"
+kvt_key_name                        = "sgtd2weustaitdjpgcomm004"
+account_tier                        = "Standard"
+access_tier                         = "Hot"
+analytics_diagnostic_monitor        = "sgtd2weustaitdjpgcomm004-dgm"
+analytics_diagnostic_monitor_blob   = "sgtd2weustaitdjpgcomm004-blo"
+account_replication_type            = "LRS"
+is_hns_enabled                      = true
+datalake_filesystem_name            = "sgtd2weustaitdjpgcomm004-dfs"
+virtual_network_subnet_ids          = []
+ip_rules                            = []
+
+# Common
+resource_group                      = "sgtd2weursgitdmodcomm001"
+cia                                 = "CLL"
+channel                             = "Intranet"
+description                         = "Test Actions"
+tracking_code                       = "ITDMOD"
+location                            = "westeurope"
---
+++ terraform/itdmodcomm001/staitdjpgcomm004/main.tf
@@ -0,0 +1,25 @@
+module "module-data-lake-storage-gen2-sm" {
+  
+  source  = "git::https://github.alm.europe.cloudcenter.corp/sgt-cloudplatform/terraform-azurerm-module-dls-sm.git?ref=v1.4.2"
+    
+  name                                = var.sta_name
+  resource_group                      = var.resource_group
+  kvt_name                            = var.kvt_name
+  kvt_key_name                        = var.kvt_key_name
+  account_tier                        = var.account_tier
+  access_tier                         = var.access_tier
+  analytics_diagnostic_monitor        = var.analytics_diagnostic_monitor
+  analytics_diagnostic_monitor_blob   = var.analytics_diagnostic_monitor_blob
+  lwk_resource_group_name             = var.resource_group
+  lwk_name                            = var.lwk_name
+  account_replication_type            = var.account_replication_type
+  is_hns_enabled                      = var.is_hns_enabled
+  datalake_filesystem_name            = var.datalake_filesystem_name
+  virtual_network_subnet_ids          = var.virtual_network_subnet_ids
+  ip_rules                            = var.ip_rules
+  cia                                 = var.cia
+  channel                             = var.channel
+  description                         = var.description
+  tracking_code                       = var.tracking_code
+
+}
